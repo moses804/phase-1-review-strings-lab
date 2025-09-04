7829a35ca04d0f273fab5cf4c9254a54b0a39439
Moses Maina <mainacalton57@gmail.com>
2025-09-04 14:19:44 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'calton';
+const welcomeMessage = 'Welcome to Flatbook, calton!';
+const shortGreeting = 'Welcome, ' + currentUser.charAt(0) + '!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
